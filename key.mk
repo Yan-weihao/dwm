cd751fe1ba58f95a25478d7e2e1aeb081fd0036a
Yan-weihao <YWHMatrix@Gmail.com>
2023-01-14 16:59:21 +0800

    添加键位的mk

---
+++ key.mk
@@ -0,0 +1,18 @@
+# Vanit
+|key|note|
+|Alt+Super+0|打开和关闭间隙|
+|Alt+Super+Shift+0|将间隙重置为默认值|
+|Alt+Super+h|增加所有差距|
+|Alt+Super+l|减少所有差距|
+|Alt+Super+Shift+h|增加外部间隙|
+|Alt+Super+Shift+l|减少外部间隙|
+|Alt+Super+Ctrl+h|增加内部间隙|
+|Alt+Super+Ctrl+l|减少内部间隙|
+|Alt+y|增加内部水平间隙|
+|Alt+o|减少内部水平间隙|
+|Alt+Ctrl+y|增加内部垂直间隙|
+|Alt+Ctrl+o|减少外部垂直间隙|
+|Alt+Super+y|增加外部水平间隙|
+|Alt+Super+o―减少外部水平间隙|
+|Alt+Shift+y|增加外部垂直间隙|
+|Alt+Shift+o|减少外部垂直间隙|
